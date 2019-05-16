f9f42ec28ed9be557c9f05982e854e053e72db3f
dpatanin <daniel.patanin@iteratec.com>
2019-05-16 13:16:00 +0200

    use the ruby gem scaffolding

--- src/main.rb
+++ src/main.rb
@@ -1,9 +1,9 @@
 require 'sinatra'
 require 'json'
-require 'rest-client'
 require 'bundler'
 Bundler.setup(:default)
 require 'ruby-scanner-scaffolding'
+require 'ruby-scanner-scaffolding/healthcheck'
 require_relative "./ssh_worker"
 
 set :port, 8080
@@ -17,45 +17,6 @@ client = SshWorker.new(
 )
 
 get '/status' do
-
-  test_run = health_check
-  status 500
-  if healthy?(client, test_run)
-    status 200
-  end
-
-	content_type :json
-	{
-		started_at: client.start_time,
-		worker_id: client.worker_id,
-		healthcheck: healthcheck(client.last_connect),
-		status: {
-			started: client.started_tasks,
-			completed: client.completed_tasks,
-			failed: client.failed_tasks
-		},
-		engine: {
-			connected_engine: client.camunda_url,
-			last_successful_connection: client.last_connect
-		},
-		scanner: {
-			version: 'latest',
-			test_run: scanner_test
-		},
-		build: {
-			repository_url: client.repository_url,
-			branch: client.branch,
-			commit_id: client.commit_id
-		}
-	}.to_json
+  Healthcheck.new(client)
 end
 
-def healthy?(worker, test_run)
-test_run == "SUCCESSFUL"
-end
-
-def health_check
-  begin
-    scanner_test.scanner_test
-  end
-end
