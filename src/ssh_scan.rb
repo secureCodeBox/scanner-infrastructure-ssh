680e2a55a3f543bce8f0ef4e43775f2e91c9aed2
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2019-08-27 11:04:03 +0200

    Add log level to scanner level.
    Otherwise the default `debug` would be used for the entire process

--- src/ssh_scan.rb
+++ src/ssh_scan.rb
@@ -6,7 +6,7 @@ require 'pathname'
 require_relative './ssh_result_transformer'
 
 $logger = Logger.new(STDOUT)
-
+$logger.level = if ENV.key? 'DEBUG' then Logger::DEBUG else Logger::INFO end
 
 class SshScan
 	attr_reader :raw_results
